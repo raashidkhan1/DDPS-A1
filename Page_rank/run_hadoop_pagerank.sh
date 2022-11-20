16514f9b0ead7a53b918435a03355a3a565ab172
raashidkhan1 <raashid.khan1@gmail.com>
2022-11-20 14:31:11 +0100

    fix path issues

--- Page_rank/run_hadoop_pagerank.sh
+++ Page_rank/run_hadoop_pagerank.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /var/scratch/ddps2204/hadoop/hadoop
+cd /var/scratch/ddps2204/hadoop
 
-bin/hdfs dfs -mkdir /input; bin/hdfs dfs -put /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/soc-Slashdot0902.txt  /input
+hdfs dfs -mkdir /input; hdfs dfs -put /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/soc-Slashdot0902.txt  /input
 
 cd etc/hadoop
 
@@ -9,20 +9,20 @@ sed -i 's:</configuration>::g'  mapred-site.xml
 
 echo '<property>
   <name>yarn.app.mapreduce.am.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/
 </value>
 </property>
 <property>
   <name>mapreduce.map.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop</value>
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/</value>
 </property>
 <property>
   <name>mapreduce.reduce.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop</value>
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/</value>
 </property>
 <property>
     <name>mapreduce.application.classpath</name>
-    <value>/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/mapreduce/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/mapreduce/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/common/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/common/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/yarn/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/yarn/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/hdfs/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/hdfs/lib/*</value>
+    <value>/var/scratch/ddps2204/hadoop/share/hadoop/mapreduce/*,/var/scratch/ddps2204/hadoop/share/hadoop/mapreduce/lib/*,/var/scratch/ddps2204/hadoop/share/hadoop/common/*,/var/scratch/ddps2204/hadoop/share/hadoop/common/lib/*,/var/scratch/ddps2204/hadoop/share/hadoop/yarn/*,/var/scratch/ddps2204/hadoop/share/hadoop/yarn/lib/*,/var/scratch/ddps2204/hadoop/share/hadoop/hdfs/*,/var/scratch/ddps2204/hadoop/share/hadoop/hdfs/lib/*</value>
 </property>
 </configuration>' >> mapred-site.xml
 
