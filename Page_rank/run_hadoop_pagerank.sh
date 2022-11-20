46a56c1a452b0947984e6c394064197940f5c515
raashidkhan1 <raashid.khan1@gmail.com>
2022-11-20 14:27:33 +0100

    update path for hadoop

--- Page_rank/run_hadoop_pagerank.sh
+++ Page_rank/run_hadoop_pagerank.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /var/scratch/hadoop
+cd /var/scratch/ddps2204/hadoop/hadoop
 
-bin/hdfs dfs -mkdir /input; bin/hdfs dfs -put /var/scratch/DDS/DDPS-A1/PageRank/soc-Slashdot0902.txt  /input
+bin/hdfs dfs -mkdir /input; bin/hdfs dfs -put /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/soc-Slashdot0902.txt  /input
 
 cd etc/hadoop
 
@@ -9,31 +9,31 @@ sed -i 's:</configuration>::g'  mapred-site.xml
 
 echo '<property>
   <name>yarn.app.mapreduce.am.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/hadoop
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop
 </value>
 </property>
 <property>
   <name>mapreduce.map.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/hadoop</value>
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop</value>
 </property>
 <property>
   <name>mapreduce.reduce.env</name>
-  <value>HADOOP_MAPRED_HOME=/var/scratch/hadoop2</value>
+  <value>HADOOP_MAPRED_HOME=/var/scratch/ddps2204/hadoop/hadoop</value>
 </property>
 <property>
     <name>mapreduce.application.classpath</name>
-    <value>/var/scratch/hadoop/share/hadoop/mapreduce/*,/var/scratch/hadoop/share/hadoop/mapreduce/lib/*,/var/scratch/hadoop/share/hadoop/common/*,/var/scratch/hadoop/share/hadoop/common/lib/*,/var/scratch/hadoop/share/hadoop/yarn/*,/var/scratch/hadoop/share/hadoop/yarn/lib/*,/var/scratch/hadoop/share/hadoop/hdfs/*,/var/scratch/hadoop/share/hadoop/hdfs/lib/*</value>
+    <value>/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/mapreduce/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/mapreduce/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/common/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/common/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/yarn/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/yarn/lib/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/hdfs/*,/var/scratch/ddps2204/hadoop/hadoop/share/hadoop/hdfs/lib/*</value>
 </property>
 </configuration>' >> mapred-site.xml
 
 
-chmod 777 /var/scratch/DDS/DDPS-A1/PageRank/mapper.py /var/scratch/DDS/DDPS-A1/PageRank/reducer.py
+chmod 777 /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/mapper.py /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/reducer.py
 
 cd ../..
 
-hadoop_performance=$(bin/mapred streaming -input /input/soc-Slashdot0902.txt  -output /ouputPR -mapper /var/scratch/DDS/DDPS-A1/PageRank/mapper.py -reducer /var/scratch/DDS/DDPS-A1/PageRank/reducer.py)
+hadoop_performance=$(bin/mapred streaming -input /input/soc-Slashdot0902.txt  -output /ouputPR -mapper /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/mapper.py -reducer /var/scratch/ddps2204/hadoop/DDS/DDPS-A1/PageRank/reducer.py)
 start = $SECONDS
-echo $hadoop_performance >> /var/scratch/DDS/PageRank/performance_log.txt
+echo $hadoop_performance >> /var/scratch/ddps2204/hadoop/DDS/PageRank/performance_log.txt
 duration = $((SECONDS - start))
 echo duration
 Footer
