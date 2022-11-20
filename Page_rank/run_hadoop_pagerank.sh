e2dc1950d9b555e78e685dd6f602c11efd23a5f1
raashidkhan1 <raashid.khan1@gmail.com>
2022-11-20 11:47:56 +0100

    updated code 2

--- Page_rank/run_hadoop_pagerank.sh
+++ Page_rank/run_hadoop_pagerank.sh
@@ -27,11 +27,11 @@ echo '<property>
 </configuration>' >> mapred-site.xml
 
 
-chmod 777 ~/DDS//DDPS-A1/PageRank/mapper.py ~/DDS//DDPS-A1/PageRank/reducer.py
+chmod 777 ~/DDS/DDPS-A1/PageRank/mapper.py ~/DDS/DDPS-A1/PageRank/reducer.py
 
 cd ../..
 
-hadoop_performance=$(bin/mapred streaming -input /input/soc-Slashdot0902.txt  -output /ouputPR -mapper ~/DDS//DDPS-A1/PageRank/mapper.py -reducer ~/DDS//DDPS-A1/PageRank/reducer.py)
+hadoop_performance=$(bin/mapred streaming -input /input/soc-Slashdot0902.txt  -output /ouputPR -mapper ~/DDS/DDPS-A1/PageRank/mapper.py -reducer ~/DDS/DDPS-A1/PageRank/reducer.py)
 start = $SECONDS
 echo $hadoop_performance >> ~/DDS/PageRank/performance_log.txt
 duration = $((SECONDS - start))
