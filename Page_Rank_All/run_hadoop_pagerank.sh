dbe4b3789e74ec20f5f2f789bd6995fbc641984b
raashidkhan1 <raashid.khan1@gmail.com>
2022-11-21 21:43:24 +0100

    update file name

--- Page_Rank_All/run_hadoop_pagerank.sh
+++ Page_Rank_All/run_hadoop_pagerank.sh
@@ -33,8 +33,8 @@ chmod 777 ~/DDPS-A1/Page_Rank_All/mapper.py ~/DDPS-A1/Page_Rank_All/reducer.py
 
 cd ../..
 
-hadoop_performance=$(bin/mapred streaming -input /input/soc-Slashdot0902.txt  -output /ouputPR -mapper ~/DDPS-A1/Page_Rank_All/mapper.py -reducer ~/DDPS-A1/Page_Rank_All/reducer.py)
 start=`date +%s`
+hadoop_performance=$(bin/mapred streaming -input /input/out.dbpedia-link  -output /ouputPR -mapper ~/DDPS-A1/Page_Rank_All/mapper.py -reducer ~/DDPS-A1/Page_Rank_All/reducer.py)
 echo $hadoop_performance >> ~/DDPS-A1/Page_Rank_All/performance_log.txt
 end=`date +%s`
 echo "Runtime was $((end-start))"
